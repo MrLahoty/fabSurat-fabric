42f3c9a27e6a6cac25f81e3c90e90ccdec00acc6
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2024-08-07 11:17:22 +0530

    update css

--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -9,7 +9,7 @@ const ProductCard = ({ product }) => {
     precision: 0.5,
   };
   return (
-    <div className="d-flex " style={{maxHeight:"400px",alignItems:"center"}}>
+    <div className="d-flex" style={{maxHeight:"400px",alignItems:"center"}}>
     <Link className="productCard d-flex" to={`/product/${product._id}`}>
       <div className="col-12 d-flex m-0 mt-3" style={{height:"200px", alignItems:"center",justifyContent:"center"}}>  <img src={product.images[0].url} alt={product.name} style={{height:"100%", width:"200px"}}/></div>
     <div className="col-12 d-flex flex-column m-0 p-4"> <p className="col-12 m-0 ">{product.name}</p>
