e9292af7819c38d552fbf5afb8bca2a2d65cabaa
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2024-09-17 19:47:24 +0530

    update css

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const app = require("./app");
+const cloudinary = require("cloudinary");
 const connectDatabase = require("./config/database");
 
 // Handling Uncaught Exception
@@ -15,6 +16,12 @@ if (process.env.NODE_ENV !== "PRODUCTION") {
 
 // Connecting to database
 connectDatabase();
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
+});
 
 const server = app.listen(process.env.PORT, () => {
     console.log(`Server is working on http://localhost:${process.env.PORT}`);
