e80dac92531eef656fc7098949247acdb4a5dbbc
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2025-01-27 13:10:28 +0530

    loveeee

--- frontend/src/component/Admin/ProductList.js
+++ frontend/src/component/Admin/ProductList.js
@@ -18,11 +18,9 @@ import { DELETE_PRODUCT_RESET } from "../../constants/productConstants";
 
 const ProductList = ({ history }) => {
   const dispatch = useDispatch();
-
   const alert = useAlert();
 
   const { error, products } = useSelector((state) => state.products);
-
   const { error: deleteError, isDeleted } = useSelector(
     (state) => state.product
   );
@@ -52,8 +50,25 @@ const ProductList = ({ history }) => {
   }, [dispatch, alert, error, deleteError, history, isDeleted]);
 
   const columns = [
-    { field: "id", headerName: "Product ID", minWidth: 200, flex: 0.5 },
-
+    {
+      field: "image",
+      headerName: "Image",
+      minWidth: 120,
+      flex: 0.4,
+      renderCell: (params) => (
+        <img
+          src={params.getValue(params.id, "image")}
+          alt="Product"
+          style={{ width: "70px", height: "50px", objectFit: "cover" }}
+        />
+      ),
+    },
+    { 
+       field: "id",
+       headerName: "Product ID",
+       minWidth: 230,
+       flex: 0.5,
+    },
     {
       field: "name",
       headerName: "Name",
@@ -67,20 +82,18 @@ const ProductList = ({ history }) => {
       minWidth: 150,
       flex: 0.3,
     },
-
     {
       field: "price",
       headerName: "Price",
       type: "number",
-      minWidth: 270,
+      minWidth: 200,
       flex: 0.5,
     },
-
     {
       field: "actions",
       flex: 0.3,
       headerName: "Actions",
-      minWidth: 150,
+      minWidth: 170,
       type: "number",
       sortable: false,
       renderCell: (params) => {
@@ -109,6 +122,7 @@ const ProductList = ({ history }) => {
     products.forEach((item) => {
       rows.push({
         id: item._id,
+        image: item.images[0].url, // Assuming images[0].url holds the product image
         stock: item.Stock,
         price: parseFloat(item.price).toFixed(2), // Formatting price
         name: item.name,
