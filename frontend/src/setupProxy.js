3dd24ea0587761db932acae426d6775e94ec3071
MrLahoty <126816363+MrLahoty@users.noreply.github.com>
2025-03-12 16:29:56 +0530

    loveeee

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api",
     createProxyMiddleware({
-      target: "http://46.202.162.95:4000",
+      target: "http://localhost:4000",
       changeOrigin: true,
     })
   );
